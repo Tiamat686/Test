8cb9463de82327638059a53cf168d13bc77766d4
Tiamat686 <92630889+Tiamat686@users.noreply.github.com>
2026-04-19 17:56:36 +0300

    Implement full gather-return loop

--- scripts/units/worker.gd
+++ scripts/units/worker.gd
@@ -1,12 +1,39 @@
 extends "res://scripts/units/unit.gd"
 
+enum State { IDLE, MOVING_TO_RESOURCE, GATHERING, RETURNING }
+
+var state = State.IDLE
 var carrying := 0
 var max_capacity := 100
 
-func gather(resource):
-	if carrying < max_capacity:
-		carrying += 10
+var target_resource = null
+var town_hall = null
 
-func deposit():
-	ResourceManager.add_gold(carrying)
-	carrying = 0
+func set_town_hall(th):
+	town_hall = th
+
+func gather_from(resource):
+	target_resource = resource
+	state = State.MOVING_TO_RESOURCE
+	move_to(resource.global_position)
+
+func _physics_process(delta):
+	super._physics_process(delta)
+
+	match state:
+		State.MOVING_TO_RESOURCE:
+			if position.distance_to(target_resource.global_position) < 10:
+				state = State.GATHERING
+
+		State.GATHERING:
+			if carrying < max_capacity:
+				var mined = target_resource.harvest(10)
+				carrying += mined
+			else:
+				state = State.RETURNING
+				move_to(town_hall.global_position)
+
+		State.RETURNING:
+			if position.distance_to(town_hall.global_position) < 10:
+				town_hall.deposit(self)
+				state = State.IDLE
