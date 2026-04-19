f6259c8039b9087bd01c0b06d0ac9f9f9b070539
Tiamat686 <92630889+Tiamat686@users.noreply.github.com>
2026-04-19 18:07:25 +0300

    Add selection switching and context commands

--- scripts/selection_manager.gd
+++ scripts/selection_manager.gd
@@ -3,8 +3,21 @@ extends Node
 var selected_unit = null
 
 func select(unit):
+	if selected_unit and selected_unit.has_method("on_selection_changed"):
+		selected_unit.on_selection_changed(false)
 	selected_unit = unit
+	if selected_unit and selected_unit.has_method("on_selection_changed"):
+		selected_unit.on_selection_changed(true)
+
+func clear_selection():
+	if selected_unit and selected_unit.has_method("on_selection_changed"):
+		selected_unit.on_selection_changed(false)
+	selected_unit = null
 
 func command_move(position: Vector2):
 	if selected_unit:
 		selected_unit.move_to(position)
+
+func command_gather(resource):
+	if selected_unit and selected_unit.has_method("gather_from"):
+		selected_unit.gather_from(resource)
