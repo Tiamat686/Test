42b2a42568283dc7de1df20868bf11617f5a5bb7
Tiamat686 <92630889+Tiamat686@users.noreply.github.com>
2026-04-19 18:52:13 +0300

    Add raycast input and RTS control

--- scripts/core/game.gd
+++ scripts/core/game.gd
@@ -1,4 +1,39 @@
 extends Node3D
 
+@onready var camera = $Camera3D
+@onready var peasant = $Peasant
+@onready var mine = $GoldMine
+@onready var town = $TownHall
+
 func _ready():
 	print("Stonecraft Game started (3D mode)")
+	peasant.SetMine(mine)
+	peasant.SetTownHall(town)
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var ray_origin = camera.project_ray_origin(event.position)
+		var ray_dir = camera.project_ray_normal(event.position)
+		var space = get_world_3d().direct_space_state
+
+		var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_origin + ray_dir * 1000)
+		var result = space.intersect_ray(query)
+
+		if result:
+			var collider = result["collider"]
+
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				if collider.get_parent() == peasant:
+					SelectionManager.select_single(peasant)
+				else:
+					SelectionManager.clear_selection()
+
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				var selected = SelectionManager.get_primary_selection()
+				if not selected:
+					return
+
+				if collider.get_parent() == mine:
+					selected.Gather()
+				else:
+					selected.MoveTo(result["position"])
